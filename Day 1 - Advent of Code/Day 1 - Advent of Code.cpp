b916773c1fafbd9db6fe06e2763ac018f62a5519
Bruno Vasconcelos <bruno.vasconcelos.dk@gmail.com>
2023-12-02 00:16:53 -0300

    day 1 - part 2 - DEATH BY A THOUSAND EDGECASES

--- Day 1 - Advent of Code/Day 1 - Advent of Code.cpp
+++ Day 1 - Advent of Code/Day 1 - Advent of Code.cpp
@@ -7,8 +7,36 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
+int extractDigits(std::string line);
+std::string swapWrittenNumbers(std::string line);
+
+int main() {
+  std::ifstream inputFile("input.txt");
+
+  if (!inputFile) {
+    std::cerr << "Well shit";
+    return 1;
+  } else {
+    std::cout << "File read successfully \n";
+  }
+
+  std::string line{};
+  int totalValue{};
+  while (inputFile >> line) {
+    std::cout << "-----------------------"
+              << "\n";
+    std::cout << line << "\n";
+    totalValue += extractDigits(swapWrittenNumbers(line));
+  }
+  std::cout << "\n We Done here \n The Total Value is " << totalValue;
+
+  return 0;
+}
+
+// Part 1
 int extractDigits(std::string line) {
   std::string combinedDigits{};
 
@@ -27,30 +55,26 @@ int extractDigits(std::string line) {
   }
 
   std::cout << "Sum of of both values is: " << combinedDigits << "\n";
-
   return std::stoi(combinedDigits);
 }
 
-int main() {
-  std::ifstream inputFile("input.txt");
+// Part 2
+std::string swapWrittenNumbers(std::string line) {
+  std::string replaced{line};
+  std::unordered_map<std::string, std::string> numbers;
 
-  if (!inputFile) {
-    std::cerr << "Well shit";
-    return 1;
-  } else {
-    std::cout << "File read successfully \n";
+  numbers = {{"o1e", "one"},     {"t2o", "two"},     {"th3ee", "three"},
+             {"fo4ur", "four"},  {"fi5ve", "five"},  {"s6x", "six"},
+             {"se7en", "seven"}, {"ei8ht", "eight"}, {"ni9ne", "nine"}};
+
+  int strIndex{};
+  for (auto x : numbers) {
+    while ((strIndex = replaced.find(x.second)) != std::string::npos) {
+      std::cout << "match found starting at index: " << strIndex << "\n";
+      replaced.replace(strIndex, x.second.length(), x.first);
+      std::cout << replaced << "\n";
+    }
   }
 
-  std::string line{};
-  int totalValue{};
-  while (inputFile >> line) {
-    std::cout << "-----------------------"
-              << "\n";
-    // inputFile >> line;
-    std::cout << line << "\n";
-    totalValue += extractDigits(line);
-  }
-  std::cout << "\n We Done here \n The Total Value is " << totalValue;
-
-  return 0;
+  return replaced;
 }
